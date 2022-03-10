18e4a6e5153f674c64fcaf7b86734a4f6c5edb5c
anshsachdeva <anshsachdeva.work@gmail.com>
2022-03-10 13:33:10 +0530

    support single repo outputs [UPLOAD]

---
+++ dog_network/src/test/java/work/curioustools/dog_network/GlideUtilTest.java
@@ -0,0 +1,12 @@
+package work.curioustools.dog_network;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GlideUtilTest {
+
+    @Test
+    public void loadImageFromInternet() {
+        Assert.assertArrayEquals(new Integer[]{12,3},new Integer[]{12,3});
+    }
+}

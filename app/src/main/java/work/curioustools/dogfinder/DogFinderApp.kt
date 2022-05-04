b01025baa81ab1952c530ef37eb6c7e86f0ff4fd
ansh <ansh@clevertap.com>
2022-05-04 15:13:48 +0530

    abc

--- app/src/main/java/work/curioustools/dogfinder/DogFinderApp.kt
+++ app/src/main/java/work/curioustools/dogfinder/DogFinderApp.kt
@@ -3,4 +3,11 @@ package work.curioustools.dogfinder
 import android.app.Application
 
 class DogFinderApp :Application(){
+
+
+    override fun onCreate() {
+        super.onCreate()
+        print("hello")
+    }
+
 }
